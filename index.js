f8339049d2d22ef87092f9e71b4b744d57d32d68
ram kumar karuppusamy <ramkumark1@outlook.com>
2023-01-16 14:37:29 +0530

    Add markup and styling for awesome books page
    1.Add markup and styling for the webpage
    2.Add navigation functionality to the webpage using javaswcript module

---
+++ index.js
@@ -0,0 +1,6 @@
+import PageNavigation from './modules/pagenavigation.js';
+
+window.onload=()=>{
+  const pageNav = new PageNavigation();
+  pageNav.displayBooks;
+}
---
+++ modules/pagenavigation.js
@@ -0,0 +1,34 @@
+export default class PageNavigation {
+  constructor() {
+    this.listbtn = document.getElementById('list-books-link');
+    this.addBookBtn = document.getElementById('add-books-link');
+    this.contactBtn = document.getElementById('contact-link');
+
+    this.listbtn.onclick = () => this.displayBooks();
+    this.addBookBtn.onclick = () => this.displayAddBooks();
+    this.contactBtn.onclick = () => this.displayContact();
+
+    this.displayBk = document.querySelector('.display-book');
+    this.addBk = document.querySelector('.add-book');
+    this.contact = document.querySelector('.contact-section');
+  }
+
+  displayBooks() {
+    // loadBooksList();
+    this.displayBk.style.display = 'block';
+    this.addBk.style.display = 'none';
+    this.contact.style.display = 'none';
+  }
+
+  displayAddBooks() {
+    this.displayBk.style.display = 'none';
+    this.addBk.style.display = 'block';
+    this.contact.style.display = 'none';
+  }
+
+  displayContact() {
+    this.displayBk.style.display = 'none';
+    this.addBk.style.display = 'none';
+    this.contact.style.display = 'block';
+  }
+}
